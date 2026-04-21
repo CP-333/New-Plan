501d5d883872e323bf189408cb68e589b5ade28c
Guo <1115166303@qq.com>
2026-04-22 02:27:00 +0800

    day1 upgraded with input and fstring

--- 01_python_basics/day1_intro.py
+++ 01_python_basics/day1_intro.py
@@ -1,5 +1,5 @@
-name = "guo"
-age = 30
-goal = "AI"
+name = input("请输入你的名字：")
+age = int(input("请输入你的年龄："))
+goal = input("请输入你的目标：")
 
-print("你好，我叫", name, "，今年", age, "岁，我想学", goal)
+print(f"你好，我叫{name}, 今年{age}岁, 我想学{goal}")
